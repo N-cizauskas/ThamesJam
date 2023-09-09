be4f65b1d209f9851c1f199b7aee4b5fb463ef5a
Joehendev <141341728+Joehendev@users.noreply.github.com>
2023-09-10 00:59:59 +0100

    Movement Complete
    Updated acceleration, can be cganged manually at any time now

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,44 +1,106 @@
-using System.Collections;
+
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerRun : MonoBehaviour
 {
-    private float horizontal;
-    private float vertical;
-    private float speed = 3f;
-    
-    private bool isFacingRight = true;
-    private float drag = 0.8f;
+	
+	
 
-    [SerializeField] private Rigidbody2D rb;
-    
-
-    void Update()
-    {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-        // if
-        //
-        // horizontal = 
-              Flip();
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
-    }
+	
+	public Rigidbody2D RB { get; private set; }
+	public bool IsFacingRight { get; private set; }
+	private Vector2 _moveInput;
+	public float runAccelAmount = 0.1F;
+	public float runDeccelAmount = 0.05F;
+	public float runMaxSpeed = 5F;
 
 
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
-    }
+
+	private void Awake()
+	{
+		RB = GetComponent<Rigidbody2D>();
+	}
+
+	private void Start()
+	{
+		IsFacingRight = true;
+	}
+
+	private void Update()
+	{
+
+
+		_moveInput.x = Input.GetAxisRaw("Horizontal");
+		_moveInput.y = Input.GetAxisRaw("Vertical");
+		if (_moveInput.x != 0)
+		{ CheckDirectionToFace(_moveInput.x > 0); }
+
+		
+	}
+	
+	
+	
+
+	private void FixedUpdate()
+	{
+		Run();
+	}
+
+	
+	private void Run()
+	{
+		
+		float targetSpeedx = _moveInput.x * runMaxSpeed;
+		float targetSpeedy = _moveInput.y * runMaxSpeed;
+		float accelRatex;
+		float accelRatey;
+
+
+		accelRatex = (Mathf.Abs(targetSpeedx) > 0.01f) ? runAccelAmount : runDeccelAmount;
+		accelRatey = (Mathf.Abs(targetSpeedy) > 0.01f) ? runAccelAmount : runDeccelAmount;
+
+
+
+
+
+		float speedDifx = targetSpeedx - RB.velocity.x;
+		float movementx = speedDifx * accelRatex;
+
+		float speedDify = targetSpeedy - RB.velocity.y;
+		float movementy = speedDify * accelRatey;
+
+		Vector2 acceleration = new Vector2(movementx, movementy);
+
+		//Convert this to a vector and apply to rigidbody
+		//RB.AddForce(acceleration, ForceMode2D.Force);
+		RB.velocity = new Vector2(RB.velocity.x + (Time.deltaTime * movementx), RB.velocity.y + (Time.deltaTime * movementy));
+		//RB.AddForce(movementy * Vector2.up, ForceMode2D.Force);
+		/*
+		 * For those interested here is what AddForce() will do
+		 * RB.velocity = new Vector2(RB.velocity.x + (Time.fixedDeltaTime  * speedDif * accelRate) / RB.mass, RB.velocity.y);
+		 * Time.fixedDeltaTime is by default in Unity 0.02 seconds equal to 50 FixedUpdate() calls per second
+		*/
+	}
+
+	private void Turn()
+	{
+		//stores scale and flips the player along the x axis, 
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+
+		IsFacingRight = !IsFacingRight;
+	}
+	
+
+
+	#region CHECK METHODS
+	public void CheckDirectionToFace(bool isMovingRight)
+	{
+		if (isMovingRight != IsFacingRight)
+			Turn();
+	}
+	#endregion
 }
-
